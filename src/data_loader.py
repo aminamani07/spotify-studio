da9e06a6f02ab7b2a8a7201cfe89d0b75ab311fd
aminamani07 <aminamani2007@gmail.com>
2026-06-10 22:09:42 +0330

    start writting data loader class in data loader file .

---
+++ src/data_loader.py
@@ -0,0 +1,9 @@
+from song import Song
+class data_loader :
+    def __init__(self , file_path):
+        self.file_path = file_path
+        self.track_list = self.track_list
+    def read_data(self):
+        pass
+    def append_track(self , new_song):
+        pass
