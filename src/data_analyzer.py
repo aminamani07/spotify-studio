03b6cb1998679dfd5bdd4b5d0eb65cf693773cf0
aminamani07 <aminamani2007@gmail.com>
2026-06-12 12:04:51 +0330

    write the analyze_maximum method function in analyze class to claculate the maximum of same variable features in tracks list .

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -20,3 +20,11 @@ class Dataanalyzer :
         nt2 = np.array(t2)
         return_val = np.median(nt2)
         return return_val
+    def analyze_maximum(self , feature_name) :
+        t2 = []
+        for r in self.track_list :
+            i = getattr(r , feature_name)
+            t2.append(float(i))
+        nt2 = np.array(t2)
+        return_val = np.amax(nt2)
+        return return_val
