1af1dd080d4017e2aa5890070681f472b285ff1e
aminamani07 <aminamani2007@gmail.com>
2026-06-12 23:56:45 +0330

    write the first method of this class and its boxplot . and creating the boxplot with the features that you enter .

--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -3,5 +3,16 @@ import matplotlib.pyplot as plt
 class Datavisualizer :
     def __init__(self , track_list):
         self.track_list = track_list
-    def popularity_boxplot(self , title , feature_name):
-       
+    def boxplot(self , title , feature_name):
+        t2 = []
+        for r in self.track_list :
+            i = getattr(r , feature_name)
+            t2.append(float(i))
+        t = feature_name.upper() + " : " + title
+        plt.clf()
+        plt.boxplot(t2)
+        plt.title(t)
+        plt.ylabel(feature_name)
+        plt.xlabel("Data Distribution")
+        plt.show()
+        
