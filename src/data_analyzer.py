a734d7561cc38be82b9ced1eb1fef0ca05087f00
aminamani07 <aminamani2007@gmail.com>
2026-06-12 11:53:09 +0330

    write the analyze_mean method function in the analyze class .

--- src/data_analyzer.py
+++ src/data_analyzer.py
@@ -4,5 +4,12 @@ import numpy as np
 class Dataanalyzer :
     def __init__(self , track_list):
         self.track_list = track_list
+    def analyze_mean(self , feature_name) :
+        t2 = []
+        for r in self.track_list :
+            i = getattr(r , feature_name)
+            t2.append(float(i))
+        nt2 = np.array(t2)
+        return_val = np.mean(nt2)
+        return return_val
     
-        
