8b2affdab0e3f594fcc4554ca73feb766720a913
aminamani07 <aminamani2007@gmail.com>
2026-06-13 19:43:52 +0330

    add the first load part to main function .

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
 from src.song import Song
 from src.data_loader import Data_loader
 from src.data_analyzer import Dataanalyzer
 from src.data_visualizer import Datavisualizer
 from src.data_cleaner import MeanImputer , MedianImputer , KNNImputer , BaseImputer , BaseOutlierHandler , ZScoreOutlierHandler , IQROutlierHandler
+
+def main():
+    f_path = "data/dataset.csv"
+    if not os.path.exists(f_path):
+        print(f"file {f_path} doesn't exist")
+        return 
+    else :
+        print("loading... pleas wait...")
+        loader = Data_loader(f_path)
+        loader.read_data()
+        print(f"load sucsessfull ... we can load {len(loader.track_list)} track from your file . ")
+
+
+
+
+
+if __name__ == "__main__" :
+    main()
