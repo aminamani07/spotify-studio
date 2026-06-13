d6d83207754beb85694eaa6f54c7d762240d2fd0
aminamani07 <aminamani2007@gmail.com>
2026-06-13 11:46:33 +0330

    add legend part to scatter plot part .

--- src/data_visualizer.py
+++ src/data_visualizer.py
@@ -18,18 +18,25 @@ class Datavisualizer :
     def scatterplot(self , feature_x , feature_y ):
         tx = []
         ty = []
+        genres = []
         for r in self.track_list :
             i = getattr(r , feature_x)
             j = getattr(r , feature_y)
+            g = getattr(r ,"genre")
             tx.append(float(i))
             ty.append(float(j))
+            genres.append(g)
+        u_genres = list(set(genres))
+        color_ind = [u_genres.index(g) for g in genres]
         t = "Scatter plot ; " + feature_x + " vs " + feature_y
         plt.clf()
-        plt.scatter(tx , ty)
+        plt.scatter(tx , ty , c=color_ind , cmap='viridis' , alpha=0.7)
         plt.title(t)
         plt.xlabel(feature_x)
         plt.ylabel(feature_y)
+        plt.legend(handles=scatter.legend_elements()[0], labels=u_genres , title="Genres")
         plt.show()
+    
 
 
 
