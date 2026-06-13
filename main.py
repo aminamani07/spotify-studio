f83a2ac7dda4ec56709486a6cd387b02b17027f8
aminamani07 <aminamani2007@gmail.com>
2026-06-13 22:08:53 +0330

    write the code for first part in main : replacing missing values .

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.data_visualizer import Datavisualizer
 from src.data_cleaner import MeanImputer , MedianImputer , KNNImputer , BaseImputer , BaseOutlierHandler , ZScoreOutlierHandler , IQROutlierHandler
 
 def main():
+    features = []
     f_path = "data/dataset.csv"
     if not os.path.exists(f_path):
         print(f"file {f_path} doesn't exist")
@@ -18,7 +19,7 @@ def main():
         print("================================\n*** WELCOM TO SPOTIFY STUDIO ***\n=====================================\n")
         while(True):
             print("pleas choose one of these items : \n1 . handle missing values (imputation)\n2 . handle outliers (clipping)\n3 . display statistical summary\n4 . data stroytelling & visualization\n5 . add a new track to dataset\n6 . reset dataset (reload original data)\n7 . save & exit")
-            n = int(input("\nyour choice (1 - 6) : ").strip())
+            n = int(input("\nyour choice (1 - 7) : ").strip())
             print("-" * 40)
             if n == 7 :
                 loader.save_data()
@@ -27,7 +28,40 @@ def main():
                     print("goodbye dear ... nice to see you . ")
                     break
             elif n == 1 :
-                pass
+                while(True)
+                    print("pleas choose one of this items that you want : \n1 . fulling missing values with using Mean (one feature) \n2 . fulling missing values with using Median (one feature) \n3 . fulling missing values with using LNN or machine learing (all features)")
+                    ch = int(input("pleas enter your choice : ").strip())
+                    if ch == 1 :
+                        fe = input("enter your feature : ").strip()
+                        if fe.lower() in features :
+                            imputer = MeanImputer()
+                            imputer.impute(loader.track_list , fe)
+                            print(f"missing values in {fe} replaced using Mean")
+                        else :
+                            print("invalid feature !!!")
+                        ex = input("do you want to exit this part and go back ? (y/n) : ").strip()
+                        if ex in ["y","yes","Yes"]:
+                            break
+                    elif ch == 2 :
+                        fe = input("enter your feature : ").strip()
+                        if fe.lower() in features :
+                            imputer = MedianImputer()
+                            imputer.impute(loader.track_list , fe)
+                            print(f"missing values in {fe} replaced using Median")
+                        else :
+                            print("invalid feature !!!")
+                        ex = input("do you want to exit this part and go back ? (y/n) : ").strip()
+                        if ex in ["y","yes","Yes"]:
+                            break
+                    elif ch == 3 :
+                        imputer = KNNImputer()
+                        imputer.impute(loader.track_list)
+                        print(f"missing values replaced using KNN and machine learing")
+                        ex = input("do you want to exit this part and go back ? (y/n) : ").strip()
+                        if ex in ["y","yes","Yes"]:
+                            break
+                    else :
+                        print("invalid choice ... pleas try again . ")
             elif n == 2 :
                 pass
             elif n == 3 :
